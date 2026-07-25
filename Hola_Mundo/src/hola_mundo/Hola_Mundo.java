0015f6b5fc92219cb43d221f1eba2f59400e67d4
KathyValladares <Kathy04Valladares@outlook.com>
2026-07-24 21:04:11 -0600

    HolaMundo

--- Hola_Mundo/src/hola_mundo/Hola_Mundo.java
+++ Hola_Mundo/src/hola_mundo/Hola_Mundo.java
@@ -15,6 +15,46 @@ public class Hola_Mundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    /*
+        1. Imprimir en pantalla 
+          a.Salidas formateadas 
+          b.Caracteres de Escape
+        2. Captura de datos 
+        
+        */
+          System.out.println("hola..me llamo Katherine");
+          System.out.println("Este sera mi 1er");
+          System.out.println("PROGRAMA!!!!!!");
+          
+          System.out.print("Vamos camino a");
+          System.out.print(" la clase de");
+          System.out.print("Progra!!");
+          
+          System.out.println("");
+          
+          System.out.printf("Tu apellido es %S\n" , "Valladares");
+          System.out.println("");
+          //alt + 92 ->\ 
+          
+          System.out.printf("%s tu apellido es\n" , "Valladares");
+          System.out.println("");
+          
+          System.out.println("Hola \n este es \n un nuevo \n amanecer");
+          System.out.println("Hola \t este es \t un nuevo \t amanecer");  
+            
+          //Variables 
+          int edad;     // tipo entero
+          double peso; // tipo decimal
+          char inicial 
+          
+          
+          
+          System.out.printf("Tu apellido %S es\n" , "Valladares");
+          
+         
+          
+          
+          }//Fin de main
+   
     
 }
